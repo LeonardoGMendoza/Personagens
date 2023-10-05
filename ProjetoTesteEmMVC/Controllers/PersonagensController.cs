079867da44446e18cb2be652ed24e17d5aee34e1
LeonardoGMendoza <desenvolvimento3000@outlook.com>
2023-10-05 05:29:00 -0300

    Implementei a parte da conexao e crie o migration

---
+++ ProjetoTesteEmMVC/Controllers/PersonagensController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoTesteEmMVC.Data;
+using ProjetoTesteEmMVC.Models;
+
+namespace ProjetoTesteEmMVC.Controllers
+{
+    public class PersonagensController : Controller
+    {
+        private readonly BancoContext db;
+
+        public PersonagensController(BancoContext conect)
+        {
+            db = conect;
+        }
+        public IActionResult Index()
+        {
+            return View(db.PersonagensDb.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult CriarPersonagem()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult CriarPersonagem(PersonagensModel persona)
+        {
+           
+            db.PersonagensDb.Add(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+          
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+           
+                var user = db.PersonagensDb.Find(id);
+                db.PersonagensDb.Remove(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+           
+        }
+    }
+}
---
+++ ProjetoTesteEmMVC/Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoTesteEmMVC.Models;
+
+
+namespace ProjetoTesteEmMVC.Data
+{
+    public class BancoContext : DbContext
+    {
+        private const string Options = "ConnectionDB";
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PersonagensModel> PersonagensDb { get; set; }
+    }
+}
